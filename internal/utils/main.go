1fc502473a6d198d6a14e7bbd55b2131723d59e6
Yusuf Musleh <yusuf.musleh@gmail.com>
2024-11-22 10:47:34 +0300

    fix: Lowercase all subdomains

--- internal/utils/main.go
+++ internal/utils/main.go
@@ -16,7 +16,7 @@ import (
 func ExtractSubdomain(host string) string {
 	splitDomain := strings.Split(host, ".")
 	subdomains := splitDomain[:len(splitDomain)-1]
-	return strings.Join(subdomains, ".")
+	return strings.ToLower(strings.Join(subdomains, "."))
 }
 
 func ExtractIP(remoteAddr string) string {
